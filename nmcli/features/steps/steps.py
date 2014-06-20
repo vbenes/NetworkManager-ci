fe7c7744efc239608d889a0d3f6081588d6fb7e8
Vladimir Benes <vbenes@redhat.com>
2014-06-20 12:52:49 +0200

    error checks less strict
    Change-Id: I2e51fc6d197d5cc046f105c79f1cc850035bfaa4

--- nmcli/features/steps/steps.py
+++ nmcli/features/steps/steps.py
@@ -290,12 +290,12 @@ def expect(context, what):
 
 @step(u'Error type "{type}" shown in editor')
 def check_error_in_editor(context, type):
-    context.prompt.expect("'%s'" % type)
+    context.prompt.expect("%s" % type)
 
 
 @step(u'Error type "{type}" while saving in editor')
 def check_error_while_saving_in_editor(context, type):
-    context.prompt.expect("'%s'" % type)
+    context.prompt.expect("%s" % type)
 
 
 @step(u'Fail up connection "{name}" for "{device}"')
