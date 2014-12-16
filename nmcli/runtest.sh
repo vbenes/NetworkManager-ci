b328f5e1d03a81f6aa77bf321d7ad125ca55a746
Vladimir Benes <vbenes@redhat.com>
2014-12-16 11:46:22 +0100

    correct profile in runtest for dhcp losing range workaround
    Change-Id: If499eccf104d3b9a3c58254f596abff39de62485

--- nmcli/runtest.sh
+++ nmcli/runtest.sh
@@ -16,7 +16,7 @@ if [ -e /tmp/nm_veth_configured ]; then
     fi
 
     if ! nmcli con s dhcp-srv |grep 192.168.100.1; then
-        nmcli con mod dhcp ipv4.addresses 192.168.100.1/24 ipv4.method shared
+        nmcli con mod dhcp-srv ipv4.addresses 192.168.100.1/24 ipv4.method shared
         nmcli c up id dhcp-srv
     fi
 
