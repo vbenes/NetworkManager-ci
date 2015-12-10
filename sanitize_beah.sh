0708e29ef4616fc530e39606b3cbc540486ee7cb
Vladimir Benes <vbenes@redhat.com>
2015-12-10 19:34:29 +0100

    quick sanitize beah with beah restart
    Change-Id: Ife2dfd2fa061a6e1bce4ff1be6fd84e1aac3e4b8

--- sanitize_beah.sh
+++ sanitize_beah.sh
@@ -1,25 +1,21 @@
 #! /bin/bash
 set -x
-nmcli con down testeth0
-sleep 2
-nmcli con up testeth0
+sudo sysctl net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl net.ipv6.conf.all.disable_ipv6=0
 
-# sudo sysctl net.ipv6.conf.all.disable_ipv6=1
-# sudo sysctl net.ipv6.conf.all.disable_ipv6=0
+nmcli dev disconnect eth0
+nmcli con del testeth0
+sleep 1
+nmcli connection add type ethernet ifname eth0 con-name testeth0
+nmcli connection modify testeth0 ipv6.may-fail no
+sudo systemctl restart NetworkManager
+sleep 5
+ip a s eth0
+ip -6 r
 
-# nmcli dev disconnect eth0
-# nmcli con del testeth0
-# sleep 2
-# systemctl restart NetworkManager
-# nmcli connection add type ethernet ifname eth0 con-name testeth0
-# nmcli con up testeth0
-# sleep 60
-# ip a s eth0
-# ip -6 r
-
-# sudo kill -9 $(ps aux|grep -v grep| grep beah-beaker-backend |awk '{print $2}')
-# sudo /usr/bin/python /usr/bin/beah-beaker-backend --log-stderr &
-# sleep 120
+sudo kill -9 $(ps aux|grep -v grep| grep beah-beaker-backend |awk '{print $2}')
+sudo /usr/bin/python /usr/bin/beah-beaker-backend --log-stderr &
+sleep 30
 
 exit 0
 
