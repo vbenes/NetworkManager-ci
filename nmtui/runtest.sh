46a59c968fea5ca717007f221f58815b5f000a7e
Vladimir Benes <vbenes@redhat.com>
2015-12-11 13:36:52 +0100

    nmtui: correct path for nmtui tests
    Change-Id: I6ab35fd147b368e2db948edb026f81186524633c

--- nmtui/runtest.sh
+++ nmtui/runtest.sh
@@ -175,9 +175,9 @@ if [ $vc -eq 1 ]; then
 elif [ $vc -eq 0 ]; then
     if [ x$TAG != x"" ]; then
         echo "Running $TAG version of $TEST"
-        behave nmcli/features -t $1 -t $TAG -k -f html -o /tmp/report_$TEST.html -f plain; rc=$?
+        behave nmtui/features -t $1 -t $TAG -k -f html -o /tmp/report_$TEST.html -f plain; rc=$?
     else
-        behave nmcli/features -t $1 -k -f html -o /tmp/report_$TEST.html -f plain; rc=$?
+        behave nmtui/features -t $1 -k -f html -o /tmp/report_$TEST.html -f plain; rc=$?
     fi
 fi
 
