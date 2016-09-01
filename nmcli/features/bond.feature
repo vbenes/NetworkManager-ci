vbenes/NetworkManager-ci

commit 8e6209bc6d654f3d0d82a0c7ffc3cd941c187e34

bond: set MTU to bond in bridge
https://bugzilla.redhat.com/show_bug.cgi?id=1364275

Change-Id: Ie167365a76e54be3b2b50fe5eedf3aa312d1859d

nmcli/features/bond.feature
@@ -1049,6 +1049,23 @@
      And "eth1:ethernet:connected:bond-slave-eth1" is visible with command "nmcli -t -f DEVICE,TYPE,STATE,CONNECTION device" in "5" seconds
 
 
+     @rhbz1364275
+     @ver+=1.4
+     @bond @bridge @bond_slaves
+     @bond_in_bridge_mtu
+     Scenario: nmcli - bond - enslave bond device to bridge and set mtu
+      * Add a new connection of type "bridge" and options "con-name bridge0 autoconnect no ifname bridge0 -- 802-3-ethernet.mtu 9000 ipv4.method manual ipv4.addresses 192.168.177.100/24 ipv4.gateway 192.168.177.1"
+      * Add a new connection of type "bond" and options "con-name bond0 autoconnect no ifname nm-bond master bridge0 -- 802-3-ethernet.mtu 9000"
+      * Add a new connection of type "ethernet" and options "con-name bond0.0 autoconnect no ifname eth1 master nm-bond -- 802-3-ethernet.mtu 9000"
+      * Add a new connection of type "ethernet" and options "con-name bond0.0 autoconnect no ifname eth1 master nm-bond -- 802-3-ethernet.mtu 9000"
+      * Bring "up" connection "bridge0"
+      * Bring "up" connection "bond0"
+      * Bring "up" connection "bond0.0"
+      Then "mtu 9000" is visible with command "ip a s eth1"
+      Then "mtu 9000" is visible with command "ip a s nm-bond"
+      Then "mtu 9000" is visible with command "ip a s bridge0"
+
+
     @testcase_281154
     @bond
     Scenario: nmcli - bond - describe bond

commit 4842126e446b893e02aecb1458fa9ab6ab78f5b4

bond: fix tags for bond_in_bridge_mtu test
s/bond_slaves/slaves/ otherwise the slaves are not deleted after the
test.

Change-Id: I22599f0571ee0d8764bc1d550e3123eda17e0dbc
Fixes: 8e6209bc6d654f3d0d82a0c7ffc3cd941c187e34

nmcli/features/bond.feature
@@ -1070,7 +1070,7 @@
 
      @rhbz1364275
      @ver+=1.4
-     @bond @bridge @bond_slaves
+     @bond @bridge @slaves
      @bond_in_bridge_mtu
      Scenario: nmcli - bond - enslave bond device to bridge and set mtu
       * Add a new connection of type "bridge" and options "con-name bridge0 autoconnect no ifname bridge0 -- 802-3-ethernet.mtu 9000 ipv4.method manual ipv4.addresses 192.168.177.100/24 ipv4.gateway 192.168.177.1"