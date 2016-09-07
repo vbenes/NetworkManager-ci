effa64bee008d6b0fc2c98b483b03ea5df7c6c02
Vladimir Benes <vbenes@redhat.com>
2016-09-07 23:54:54 +0200

    sanitization: use systemctl condreload
    Change-Id: I0ca181fa38c1ac60ccad2d90ecd02855d8fe212b

--- sanitize_beah.sh
+++ sanitize_beah.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 set -x
-sudo sysctl net.ipv6.conf.all.disable_ipv6=1
-sudo sysctl net.ipv6.conf.all.disable_ipv6=0
+#sudo sysctl net.ipv6.conf.all.disable_ipv6=1
+#sudo sysctl net.ipv6.conf.all.disable_ipv6=0
 
 nmcli dev disconnect eth0
 nmcli con del testeth0
 sleep 1
 nmcli connection add type ethernet ifname eth0 con-name testeth0
 nmcli connection modify testeth0 ipv6.may-fail no
-sudo systemctl restart NetworkManager
-sleep 5
+nmcli con up id testeth0
 ip a s eth0
 ip -6 r
 
-sudo kill -9 $(ps aux|grep -v grep| grep beah-beaker-backend |awk '{print $2}')
-sudo /usr/bin/python /usr/bin/beah-beaker-backend --log-stderr &
-sleep 60
+#sudo kill -9 $(ps aux|grep -v grep| grep beah-beaker-backend |awk '{print $2}')
+#sudo /usr/bin/python /usr/bin/beah-beaker-backend --log-stderr &
+systemctl condreload beah-beaker-backend.service
+#sleep 60
 
 exit 0
