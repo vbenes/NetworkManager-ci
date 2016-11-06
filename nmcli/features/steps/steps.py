dc4a5dd7a63299e37d2299d25d2315393a60aa23
Vladimir Benes <vbenes@redhat.com>
2016-11-06 20:02:04 +0100

    steps: sleep 0.1 after tab tab in editor
    Change-Id: I294dbea77236b00a3905d2b8859c02156d316cfa

--- nmcli/features/steps/steps.py
+++ nmcli/features/steps/steps.py
@@ -319,8 +319,8 @@ def check_describe_output_in_editor(context, options, obj):
     options = options.split('|')
     for opt in options:
         context.prompt.sendcontrol('c')
-        sleep(0.4)
         context.prompt.send('set %s \t\t' % obj)
+        sleep(0.1)
         a =  context.prompt.expect(["%s" % opt, pexpect.TIMEOUT], timeout=5)
         assert a == 0 , "Option %s was not shown!" % opt
 
