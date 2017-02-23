5b3d04965a59ca1138eaf02aa7f2f3f678ebb972
Vladimir Benes <vbenes@redhat.com>
2017-02-23 10:43:33 +0100

    steps: remove insane waits around service restarts
    Change-Id: I55bb06de98a681c8c6ece03db5bb90512a3d75e1

--- nmcli/features/steps/steps.py
+++ nmcli/features/steps/steps.py
@@ -1358,10 +1358,10 @@ def reboot(context):
         command_code(context, "sudo ip addr flush dev eth%d" %int(x))
     command_code(context, "nmcli device disconnect nm-bond")
     command_code(context, "nmcli device disconnect nm-team")
-    sleep(4)
+    sleep(2)
     context.nm_restarted = True
     assert command_code(context, "sudo service NetworkManager restart") == 0
-    sleep(4)
+    sleep(1)
 
 
 @step(u'Start NM')
@@ -1372,10 +1372,9 @@ def start_NM(context):
 
 @step(u'Restart NM')
 def restart_NM(context):
-    sleep(1)
     context.nm_restarted = True
     command_code(context, "service NetworkManager restart") == 0
-    sleep(4)
+    sleep(1)
 
 
 @step(u'Stop NM')
